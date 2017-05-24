386297edd3aad3e08742114f0a28cb331e0a2c4c
Thurston Stone <Thurston.Stone@xo.com>
2017-05-24 12:07:30 -0500

    asdf

--- winrm/tests/test_transport.py
+++ winrm/tests/test_transport.py
@@ -1,15 +1,21 @@
 # coding=utf-8
-from winrm.transport import Transport
 import os
+from winrm.transport import Transport
+
 
 def test_build_session():
-    origEnviron = os.environ
-    os.environ['REQUESTS_CA_BUNDLE'] = 'MyRequestsCA'
-    transport = Transport(endpoint="Endpoint")
-    # temporarily set the CA environment variable
+    transport = Transport(endpoint="Endpoint",
+                          server_cert_validation='validate',
+                          username='test',
+                          password='test',
+                          auth_method='basic',
+                          )
+    os.environ['REQUESTS_CA_BUNDLE'] = 'path_to_REQUESTS_CA_CERT'
     session = transport.build_session()
-    assert(session.verify == 'MyRequestsCA')
-    os.environ = origEnviron 
-    os.environ['CURL_CA_BUNDLE'] = 'MyCurlCA'
-    assert(session.verify == 'MyCurlCA')
-    os.environ = origEnviron 
+    assert(session.verify == 'path_to_REQUESTS_CA_CERT')
+    del os.environ['REQUESTS_CA_BUNDLE']
+
+    os.environ['CURL_CA_BUNDLE'] = 'path_to_CURL_CA_CERT'
+    session = transport.build_session()
+    assert(session.verify == 'path_to_CURL_CA_CERT')
+    del os.environ['CURL_CA_BUNDLE']
